a75ad289c0f45a40bfc705888c3e6d8e2b011f15
Nguyễn Văn Thọ <56969447+VANTHO15@users.noreply.github.com>
2023-07-11 15:45:25 +0700

    Update and rename 02_HuLa_feamewok.c to 02_HuLa_framework.c

--- 02_HuLa_framework.c
+++ 02_HuLa_framework.c
@@ -3,8 +3,15 @@ def dir_tool = "F:/Jenkin/00Source/FW_FULL/tool"
 pipeline {
     agent any
     stages{
-        stage("Delete Output") {
+        stage("Replace File and Delete Output") {
             steps {
+                script {
+                    def user_config = readFile(file: "${dir_tool}/UserConfig_Template.py")
+                    user_config = user_config.replace("MODULE_REPLACE","${module}")
+                    println(user_config)
+                    writeFile(file: "${dir_tool}/UserConfig.py", text: user_config)
+                    println("DONE REPLACE")
+                }
                 dir("$dir_tool") {
                     script {
                        bat "python main.py -o clean"
@@ -12,6 +19,7 @@ pipeline {
                 }
             }
         }
+        
         stage("Generate Plugin") {
             steps {
                 dir("$dir_tool") {
